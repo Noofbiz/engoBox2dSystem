07465b1c946cb788449f6fa372f6fafe8ae01fa3
Noofbiz <caligiure.ja@gmail.com>
2018-01-11 14:01:09 -0500

    Remove box stuff from physics.go

--- physics.go
+++ physics.go
@@ -8,21 +8,6 @@ import (
 	"github.com/ByteArena/box2d"
 )
 
-// Box2dComponent holds the box2d Body for use by Systems
-type Box2dComponent struct {
-	// Body is the box2d body
-	Body *box2d.B2Body
-}
-
-var listOfBodiesToRemove []*box2d.B2Body
-
-// DestroyBody destroys the box2d body from the World
-// this does it safely at the end of an Update, so no bodies are removed during
-// a simulation step, which can cause a crash
-func (b *Box2dComponent) DestroyBody() {
-	listOfBodiesToRemove = append(listOfBodiesToRemove, b.Body)
-}
-
 type box2dPhysicsEntity struct {
 	*ecs.BasicEntity
 	*common.SpaceComponent
