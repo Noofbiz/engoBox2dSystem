f6dc1444c07d209c638be4f8c2fea39b9e4f2bde
Noofbiz <caligiure.ja@gmail.com>
2018-02-10 14:28:39 -0500

    package comment

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package engoBox2dSystem provides a collision, physics, and mouse system
+// for use with the engo game engine. These systems are integrated with the
+// go port of the box2d physics engine.
+package engoBox2dSystem
